ca6d4023e3bb60cba24b6703febe6048c03e80f2
spangen87 <rikard.spangmyr@bestlas.se>
2022-05-20 12:45:05 +0000

    Add array of all the cards and add images to folder

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,22 @@
+// Array of all the cards
 
+let cardList = [
+    {name: 'bear', img: 'assets/images/bear.png'},
+    {name: 'dinosarie', img: 'assets/images/dinosarie.png'},
+    {name: 'dog', img: 'assets/images/dog.png'},
+    {name: 'elephant', img: 'assets/images/elephant.png'},
+    {name: 'horse', img: 'assets/images/horse.png'},
+    {name: 'monkey', img: 'assets/images/monkey.png'},
+    {name: 'panda', img: 'assets/images/panda.png'},
+    {name: 'unicorn', img: 'assets/images/unicorn.png'},
+    {name: 'bear', img: 'assets/images/bear.png'},
+    {name: 'dinosarie', img: 'assets/images/dinosarie.png'},
+    {name: 'dog', img: 'assets/images/dog.png'},
+    {name: 'elephant', img: 'assets/images/elephant.png'},
+    {name: 'horse', img: 'assets/images/horse.png'},
+    {name: 'monkey', img: 'assets/images/monkey.png'},
+    {name: 'panda', img: 'assets/images/panda.png'},
+    {name: 'unicorn', img: 'assets/images/unicorn.png'},
+];
+
+console.log(cardList);
